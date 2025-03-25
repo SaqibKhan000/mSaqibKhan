92aeb632a3627b3559c98e9c7e12cce14aec6a26
SaqibKhan000 <saqibkhannn543@gmail.com>
2025-03-25 08:40:57 +0500

    Fixing some issues in responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+  //For Rotating Text/////////////
 let text = document.querySelector(".circle .text");
 text.innerHTML = text.innerText.split("").map((char, i) => 
     `<span style="transform:rotate(${i * 10}deg)">${char}</span>`
@@ -8,3 +9,18 @@ firstSecText.innerHTML = firstSecText.innerText.split("").map((firstSecChar, fir
     `<span style="transform:rotate(${firstSecInd * 13}deg)">${firstSecChar}</span>`
 ).join("");
 
+
+
+
+//For Tabs ////////////////////////
+let tabs = document.querySelectorAll(".tab");
+let allContent = document.querySelectorAll(".content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+     tabs.forEach((tab) => {tab.classList.remove("active")});
+     allContent.forEach((content) => {content.classList.remove("active")});
+     tab.classList.add("active");
+     allContent[index].classList.add("active");
+  }); 
+});
